0d6a45d5810c4914957c7d6062107ea29ca17522
Vaibhav Lakhmani <vaibhavlakh000@gmail.com>
2018-09-16 23:22:20 +0530

    Added HTML, CSS and Timer
    -finished the look and design of the website.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-<script>
+i = 20;
+function onTimer() {
+	document.getElementById("start").disabled = true;
+document.getElementById('counter').innerHTML = i;
+i--;
+if (i < 0) {
+	document.getElementById("start").disabled = false;
+alert('TIMEUP!');
+clearTimeout(i);
+}
+else {
+
+setTimeout(onTimer, 1000);
+}
+}
